fec85db8b37739ace9334b20f3009ebcc47e1fe6
Testdao1998 <92204849+Testdao1998@users.noreply.github.com>
2021-10-23 14:39:55 +0700

    Add files via upload

--- Salary/SalaryCal.java
+++ Salary/SalaryCal.java
@@ -2,35 +2,43 @@ package Salary;
 
 
 import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class SalaryCal {
     public static void main(String[] args) {
         //Create 1 CEO, 1 Manager, 2 Employee
-        Scanner scanner= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        CEO ceo= new CEO();
+        CEO ceo = new CEO();
         Manager manager = new Manager();
         NormalEmployee employee1 = new NormalEmployee();
         NormalEmployee employee2 = new NormalEmployee();
 
         // Set salary
         System.out.println("Input CEO Salary");
-        double ceoSalary= scanner.nextDouble();
+        double ceoSalary = scanner.nextDouble();
         System.out.println("Input Manager Salary");
-        double managerSalary= scanner.nextDouble();
+        double managerSalary = scanner.nextDouble();
         System.out.println("Input employee1 Salary");
-        double employee1Salary= scanner.nextDouble();
+        double employee1Salary = scanner.nextDouble();
         System.out.println("Input employee2 Salary");
-        double employee2Salary= scanner.nextDouble();
+        double employee2Salary = scanner.nextDouble();
         ceo.setSalary(ceoSalary);
         manager.setSalary(managerSalary);
         employee1.setSalary(employee1Salary);
         employee2.setSalary(employee2Salary);
+        List<NormalEmployee> normalEmployees = new ArrayList<>();
+        normalEmployees.add(employee1);
+        normalEmployees.add(employee2);
 
 
         // Get monthly Salary
-double sum= ceoSalary + managerSalary + employee1Salary+ employee2Salary;
-        System.out.println("summery monthly salary: " +sum);
+        double totalSalary = ceo.getSalary() + managerSalary;
+        for (NormalEmployee emp : normalEmployees){
+           totalSalary += emp.getSalary();
+        }
+        System.out.println("summery monthly salary: " + totalSalary);
     }
 }
